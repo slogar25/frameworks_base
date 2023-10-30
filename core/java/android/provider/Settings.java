c208a644d1f204d9f6594b1d30e90f8e06adcb9e
jhenrique09 <jhenrique09.mcz@hotmail.com>
2023-10-30 18:08:40 +0530

    base: Add three-fingers-swipe to screenshot [1/2]
    Reverse engineered from Xiaomi/beryllium/beryllium:9/PKQ1.180729.001/9.2.25:user/release-keys
    
    commit 827837b0e2deb21587ac24be99ac98e5828fb400 (HEAD -> ten-plus)
    Author: jhenrique09 <jhenrique09.mcz@hotmail.com>
    Date:   Wed Oct 9 15:07:19 2019 +0000
    
        PhoneWindowManager: Disable three finger to screenshot on doze
    
        Change-Id: Ic7b38ffbe1980e78f9573dc2b0dc0b850d2c3ec2
    
    Change-Id: Iffdff674ba3419126df22150b47d1cb765b000d7

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -6145,6 +6145,12 @@ public final class Settings {
         @Readable
         public static final String LOCKSCREEN_BATTERY_INFO = "lockscreen_battery_info";
 
+	/**
+         * Swipe to screenshot
+         * @hide
+         */
+        public static final String SWIPE_TO_SCREENSHOT = "swipe_to_screenshot";
+
         /**
          * Keys we no longer back up under the current schema, but want to continue to
          * process when restoring historical backup datasets.
@@ -6279,6 +6285,7 @@ public final class Settings {
             PRIVATE_SETTINGS.add(VIBRATE_ON_CONNECT);
             PRIVATE_SETTINGS.add(VIBRATE_ON_CALLWAITING);
             PRIVATE_SETTINGS.add(VIBRATE_ON_DISCONNECT);
+            PRIVATE_SETTINGS.add(SWIPE_TO_SCREENSHOT);
         }
 
         /**
