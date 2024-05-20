833ff3bc85d4576428e9e37e4354030b94d58e7e
Libra420T <ryantf420@gmail.com>
2024-05-20 14:10:06 -0400

    Revert "SystemUI: Smart Pixels [1/2]"
    This reverts commit c2f4f30f0d1facaec1adcd84fc7607064b3f837b.

--- packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
+++ packages/SystemUI/src/com/android/systemui/dagger/SystemUICoreStartableModule.kt
@@ -52,7 +52,6 @@ import com.android.systemui.recents.Recents
 import com.android.systemui.recents.ScreenPinningRequest
 import com.android.systemui.settings.dagger.MultiUserUtilsModule
 import com.android.systemui.shortcut.ShortcutKeyDispatcher
-import com.android.systemui.smartpixels.SmartPixelsReceiver
 import com.android.systemui.statusbar.ImmersiveModeConfirmation
 import com.android.systemui.statusbar.gesture.GesturePointerEventListener
 import com.android.systemui.statusbar.notification.InstantAppNotifier
@@ -330,12 +329,6 @@ abstract class SystemUICoreStartableModule {
     @IntoMap
     @ClassKey(AssistantAttentionMonitor::class)
     abstract fun bindAssistantAttentionMonitor(sysui: AssistantAttentionMonitor): CoreStartable
-    
-    /** Inject into SmartPixelsReceiver.  */
-    @Binds
-    @IntoMap
-    @ClassKey(SmartPixelsReceiver::class)
-    abstract fun bindSmartPixelsReceiver(sysui: SmartPixelsReceiver): CoreStartable
 
     @Binds
     @IntoMap
